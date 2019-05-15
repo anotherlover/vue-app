953fe9fa630d8f01511dbe7f30322e164ccc48c2
不会谈情的九号 <1031506107@qq.com>
2019-05-15 22:47:40 +0800

    mock数据。

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,56 @@
 <template>
-    <div>
-        i'm goods
-    </div>
+  <div class="demo">
+    <p>
+      <span>email: {{mockData.email}}</span>
+    </p>
+    <p>
+      sentence: {{mockData.content}}
+    </p>
+    <p>
+      image: <img :src="mockData.dataImage"
+        alt="">
+    </p>
+    <p>
+      color:
+      <span :style="{color:mockData.color}">text color</span>
+    </p>
+    <p>
+      name: {{mockData.name}}
+    </p>
+  </div>
 </template>
 <script>
+import { Random } from 'mockjs'
 export default {
+  data() {
+    return {
+      mockData: {}
+    }
+  },
+  created() {
+    this.mockData = {
+      content: Random.sentence(),
+      image: Random.image(),
+      email: Random.email(),
+      dataImage: Random.dataImage(),
+      color: Random.color(),
+      name: Random.name()
+
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
+.demo {
+  margin-left: 20px;
+}
+
+p {
+  margin-top: 10px;
+}
+
+img {
+  width: 100px;
+  height: 100px;
+}
 </style>
